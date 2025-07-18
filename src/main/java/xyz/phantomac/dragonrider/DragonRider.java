4b121ce8212f7b287f0eb5b69cc105d4f48dc707
Away <125528637+AwayXD@users.noreply.github.com>
2025-07-17 22:42:31 -0500

    vers change

--- src/main/java/xyz/phantomac/dragonrider/DragonRider.java
+++ src/main/java/xyz/phantomac/dragonrider/DragonRider.java
@@ -19,7 +19,7 @@ public class DragonRider {
 
     public static final String MODID = "dragonrider";
     public static final String NAME = "Dragon Rider";
-    public static final String VERSION = "1.7";
+    public static final String VERSION = "1.1";
 
     private boolean enabled = true;
     private boolean circling = false;
